2e3cf8d51c8f127f0eb3af0439de870087bde7a8
anha-cs <anha@csus.edu>
2026-03-05 19:52:04 -0800

    Initial commit for Assignment 5

---
+++ fancifymytext.js
@@ -0,0 +1,30 @@
+function makeBigger() {
+    // alert("Hello, world!");
+    document.getElementById("userInput").style.fontSize = "24pt";
+}
+
+function changeStyle() {
+    // alert("Changed!")
+    let textArea = document.getElementById("userInput");
+    let isFancy = document.getElementById("fancy").checked;
+
+    if (isFancy) {
+        textArea.style.fontWeight = "bold";
+        textArea.style.color = "blue";
+        textArea.style.textDecoration = "underline";
+    } else {
+        textArea.style.fontWeight = "normal";
+        textArea.style.color = "black";
+        textArea.style.textDecoration = "none";
+
+    }
+}
+
+function moo() {
+    let textArea = document.getElementById("userInput");
+    let text = textArea.value.toUpperCase();
+
+    let parts = text.split(".");
+    text = parts.join("-Moo.");
+    textArea.value = text;
+}
